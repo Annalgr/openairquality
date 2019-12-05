f57519d2d439c5900d4468eaa193964a13df193e
FrancescaShell <francesca.griggio@student.h-farm.com>
2019-12-05 19:30:22 +0100

    Three levels of verbosity implemented

--- main.py
+++ main.py
@@ -5,13 +5,31 @@ from pypackage.openairquality import get_quality
 parser = argparse.ArgumentParser()
 parser.add_argument("city", help="Name of the city to be sought")
 parser.add_argument("-p", "--parameter", help="Polluting particles")
-parser.add_argument("-v", "--verbosity", help="Increase output verbosity", action="store_true")
+parser.add_argument("-v", "--verbosity", help="Increase output verbosity", action="count")
 args = parser.parse_args()
 
 p_value = get_quality(args.city, args.parameter)
 
-if args.verbosity:
-    print("{} has a {} value of {}".format(args.city, args.parameter, p_value))
-    
+if args.verbosity == 2:
+    print("Second level of verbosity turned on")
+
+    if p_value == -1:
+        print("Could not find a value of {} for {}".format(args.parameter, args.city))
+    else:
+        print("The city of {} has a {} value of {}".format(args.city, args.parameter, p_value))
+
+elif args.verbosity == 1:
+    print("First level of verbosity turned on")
+
+    if p_value == -1:
+        print("{} for {} not found".format(args.parameter, args.city))
+    else:
+        print("{} in {} = {}".format(args.parameter, args.city, p_value))
+
 else:
-    print(p_value)
+    if p_value == -1:
+        print("None")
+    else:
+        print(p_value)
+
+
