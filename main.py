68cf173e3b5642c4d048bf54d1adbfc1a2dadcff
FrancescaShell <francesca.griggio@student.h-farm.com>
2019-12-05 19:02:08 +0100

    City added as a positional argument

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from pypackage.openairquality import get_quality
-
-pm10=get_quality()
-print("Rome has a pm10 value of {}".format(pm10))
-no2=get_quality(city="Bologna", parameter='no2')
-if no2==-1:
-    print("Could not find a value of no2 for Bologna")
+import argparse
+from mypackage.openairquality import get_quality
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument("city", help="Name of the city to be sought")
+args = parser.parse_args()
+
+p_value = get_quality(args.city, parameter='pm10')
+print("{} has a pm10 value of {}".format(args.city, p_value))
--- pypackage/openairquality.py
+++ pypackage/openairquality.py
@@ -2,14 +2,15 @@ import requests
 import json
 
 
-openaq_URL = 'https://api.openaq.org/v1/latest?city={}&parameter={}'
+openaq_url = 'https://api.openaq.org/v1/latest?city={}&parameter={}'
 
 
-def get_quality(city="Roma", parameter='pm10'):
-    URL = openaq_URL.format(city, parameter)
-    r = requests.get(URL)
+def get_quality(city, parameter='pm10'):
+    url = openaq_url.format(city, parameter)
+    r = requests.get(url)
     data = json.loads(r.text)['results']
-    p_value=0
+    p_value = 0
+    
     if data:
         for d in data:
             measure = d['measurements'][0]
