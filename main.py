d55a1daf7e4aafe7f6e8a341f30f6b36d3616a7f
FrancescaShell <francesca.griggio@student.h-farm.com>
2019-12-05 19:15:39 +0100

    Verbosity added

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ from pypackage.openairquality import get_quality
 parser = argparse.ArgumentParser()
 parser.add_argument("city", help="Name of the city to be sought")
 parser.add_argument("-p", "--parameter", help="Polluting particles")
+parser.add_argument("-v", "--verbosity", help="Increase output verbosity", action="store_true")
 args = parser.parse_args()
 
 p_value = get_quality(args.city, args.parameter)
-print("{} has a {} value of {}".format(args.city,args.parameter, p_value))
 
+if args.verbosity:
+    print("{} has a {} value of {}".format(args.city, args.parameter, p_value))
+    
+else:
+    print(p_value)
