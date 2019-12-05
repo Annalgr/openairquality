ad6f97eed5179880a395952532bc2f7cb36750f8
FrancescaShell <francesca.griggio@student.h-farm.com>
2019-12-05 19:13:52 +0100

    Parameter added as an optional argument

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from pypackage.openairquality import get_quality
 
 parser = argparse.ArgumentParser()
 parser.add_argument("city", help="Name of the city to be sought")
+parser.add_argument("-p", "--parameter", help="Polluting particles")
 args = parser.parse_args()
 
-p_value = get_quality(args.city, parameter='pm10')
-print("{} has a pm10 value of {}".format(args.city, p_value))
+p_value = get_quality(args.city, args.parameter)
+print("{} has a {} value of {}".format(args.city,args.parameter, p_value))
+
--- pypackage/openairquality.py
+++ pypackage/openairquality.py
@@ -5,7 +5,7 @@ import json
 openaq_url = 'https://api.openaq.org/v1/latest?city={}&parameter={}'
 
 
-def get_quality(city, parameter='pm10'):
+def get_quality(city, parameter):
     url = openaq_url.format(city, parameter)
     r = requests.get(url)
     data = json.loads(r.text)['results']
@@ -17,3 +17,4 @@ def get_quality(city, parameter='pm10'):
             p_value += measure['value']
         return p_value/len(data)
     return -1
+
