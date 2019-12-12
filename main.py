d51d48322bf708da32fcbbf4c6c41786211e6c4f
MarGua21 <martina.gualandi@student.h-farm.com>
2019-12-12 18:13:10 -0500

    check for allowed users in main.py

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 import argparse
-from pypackage.openairquality import get_quality
 from pypackage.openairquality import list_csv
+from pypackage.openairquality import check_city
+from pypackage.openairquality import get_quality
+from pypackage.db_connection import check_db
 
 parser = argparse.ArgumentParser()
-parser.add_argument("city", help="Name of the city to be sought",choices = list_csv('pypackage/cities.csv'))
-parser.add_argument("-p", "--parameter", help="Polluting particles required (default=pm10)", choices = list_csv('pypackage/parameters.csv'), default="pm10")
+parser.add_argument("username", help="Username to log-in (password required)")
+parser.add_argument("-p", "--password", help="Password to log-in required", required=True)
+parser.add_argument("-c", "--city", help="Name of the European city to be sought", required=True)
+parser.add_argument("-m", "--molecule", help="Polluting molecule required (default=pm10)", choices = list_csv('pypackage/parameters.csv'), default="pm10")
 parser.add_argument("-v", "--verbosity", help="Increase output verbosity", action="count")
 args = parser.parse_args()
 
-p_value = get_quality(args.city, args.parameter)
 
-if args.verbosity == 2:
-    print("Second level of verbosity turned on")
+if check_db(args):
+ 
+    p_value = get_quality(args.city, args.molecule)
 
-    if p_value == -1:
-        print("Could not find a value of {} for {}".format(args.parameter, args.city))
+    if args.verbosity == 2:
+        print("Second level of verbosity turned on")
+
+        if p_value == -1:
+            print("Could not find a value of {} for {}".format(args.molecule, args.city))
+        else:
+            print("The city of {} has a {} value of {}".format(args.city, args.molecule, p_value))
+
+    elif args.verbosity == 1:
+        print("First level of verbosity turned on")
+
+        if p_value == -1:
+            print("{} for {} not found".format(args.molecule, args.city))
+        else:
+            print("{} in {} = {}".format(args.molecule, args.city, p_value))
+
     else:
-        print("The city of {} has a {} value of {}".format(args.city, args.parameter, p_value))
-
-elif args.verbosity == 1:
-    print("First level of verbosity turned on")
-
-    if p_value == -1:
-        print("{} for {} not found".format(args.parameter, args.city))
-    else:
-        print("{} in {} = {}".format(args.parameter, args.city, p_value))
-
-else:
-    if p_value == -1:
-        print("None")
-    else:
-        print(p_value)
+        if p_value == -1:
+            print("None")
+        else:
+            print(p_value)
---
+++ pypackage/db_connection.py
@@ -0,0 +1,37 @@
+"""Check the user is allowed to run the code."""
+
+import sqlite3
+import hashlib
+import random
+
+
+def check_db(args):            
+    # Open connection to the regisered users db
+    conn = sqlite3.connect('pypackage/openaq_users.db')
+    cursor = conn.cursor()    
+
+    # Check for username
+    row = cursor.execute("SELECT * FROM user_database WHERE username = ?",(args.username,))
+    results = row.fetchall()
+    conn.commit()
+
+    if results:   
+        # Add salt
+        salt = str(results[0][2])
+        concat = salt + args.password
+
+        # Compute the hash
+        for i in range (1000):
+            digest = hashlib.sha256(concat.encode('utf-8')).hexdigest()
+
+        # Check for password
+        if digest == results[0][1]:
+            print ('Successful log-in. Welcome {}!'.format(args.username))
+            return True
+                    
+        else:
+            return print("Password is invalid for user {}.".format(args.username))            
+    else:
+        return print ("Username not present.")
+        
+
