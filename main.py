b5591c495eb194c4fc75be7f9df50a188521cb24
Annalgr <anna.lagrasta@student.h-farm.com>
2019-12-13 12:01:17 +0100

    main.py fixed with pycodestyle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import argparse
+
 from pypackage.openairquality import list_csv
 from pypackage.openairquality import check_city
 from pypackage.openairquality import get_quality
@@ -6,17 +7,24 @@ from pypackage.db_connection import check_db
 
 parser = argparse.ArgumentParser()
 parser.add_argument("username", help="Username to log-in (password required)")
-parser.add_argument("-p", "--password", help="Password to log-in required", required=True)
-parser.add_argument("-c", "--city", help="Name of the European city to be sought", required=True)
-parser.add_argument("-m", "--molecule", help="Polluting molecule required (default=pm10)", choices = list_csv('pypackage/parameters.csv'), default="pm10")
-parser.add_argument("-v", "--verbosity", help="Increase output verbosity", action="count")
+parser.add_argument("-p", "--password",
+                    help="Password to log-in required", required=True)
+parser.add_argument("-c", "--city",
+                    help="Name of the European city to be sought",
+                    required=True)
+parser.add_argument("-m", "--molecule",
+                    help="Polluting molecule required (default=pm10)",
+                    choices=list_csv('pypackage/parameters.csv'),
+                    default="pm10")
+parser.add_argument("-v", "--verbosity", help="Increase output verbosity",
+                    action="count")
 args = parser.parse_args()
 
 
 # Check if the username is registered
 if check_db(args) and check_city(str(args.city)):
 
-    # Get air quality value   
+    # Get air quality value
     p_value = get_quality(args.city, args.molecule)
 
     # Print the result based on verbosity level
@@ -24,9 +32,14 @@ if check_db(args) and check_city(str(args.city)):
         print("Second level of verbosity turned on")
 
         if p_value == -1:
-            print("Could not find a value of {} for {}".format(args.molecule, args.city))
+            print("Could not find a value of {} for {}".format(
+                                                               args.molecule,
+                                                               args.city))
         else:
-            print("The city of {} has a {} value of {}".format(args.city, args.molecule, p_value))
+            print("The city of {} has a {} value of {}".format(
+                                                               args.city,
+                                                               args.molecule,
+                                                               p_value))
 
     elif args.verbosity == 1:
         print("First level of verbosity turned on")
